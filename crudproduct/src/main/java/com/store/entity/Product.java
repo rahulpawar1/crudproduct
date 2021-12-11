ff38e2d747d058797da9626669836c1b5a002405
Rahul <pawarrahul699@gmail.com>
2021-12-11 23:38:39 +0530

    add database config and modify the entity

--- crudproduct/src/main/java/com/store/entity/Product.java
+++ crudproduct/src/main/java/com/store/entity/Product.java
@@ -10,23 +10,53 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 //model class
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name="PRODUCT_TBL")
 public class Product {
 
 	@Id
 	@GeneratedValue //this id we need auto generated so we use @GeneratedValue annotation.
-	private int id;
-	@SuppressWarnings("unused")
+	private int id;	
 	private String name;
-	
-	@SuppressWarnings("unused")
 	private int quantity;
-	
-	@SuppressWarnings("unused")
 	private double price;
+	
+	//default constructor
+	public Product() {
+		
+	}
+	
+	public Product(int id, String name, int quantity, double price) {
+		this.id = id;
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
 	
 }
